bb8d80570b145fb03f28ae836f9d5911695e2a8f
acnunez <acnunez@tfc.com.pe>
2018-01-22 10:14:09 -0500

    acnunez [fox_zeus-R001] 22.01.2018 - inicio - Rev02

--- app/config/loader.php
+++ app/config/loader.php
@@ -6,8 +6,9 @@ $loader = new \Phalcon\Loader();
  * We're a registering a set of directories taken from the configuration file
  */
 $loader->registerDirs(
-    [
-        $config->application->controllersDir,
-        $config->application->modelsDir
-    ]
+                        [
+                                        $config->application->controllersDir,
+                                        $config->application->modelsDir,
+                                        $config->application->formsDir
+                        ]
 )->register();
--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,12 +1,30 @@
 <?php
 
-class IndexController extends ControllerBase
-{
+use LoginForm as FormLogin,
+    Phalcon\Session as Session;
+class IndexController extends ControllerBase {
 
-    public function indexAction()
-    {
-
+    public function onConstruct() {
+        
     }
 
-}
+    private function _registerSession($usuario) {
+        /* $parametrosGenerales = parent::obtenerParametros('TIME_OUT_SESSION');
 
+          $this->session->set('Usuario',
+          array(  'codUsuario'    => $usuario->codUsuario,
+          'nombreUsuario' => $usuario->nombreUsuario,
+          'codEmpresa'    => $usuario->codEmpresa,
+          'nombresPersona'=> $usuario->nombresPersona,
+          'nombreEmpresa' => $usuario->nombreEmpresa,
+          'tiempoSesion' => $parametrosGenerales,
+          'ultimoAcceso' => date("Y-n-j H:i:s"),
+          'indicadorUsuarioAdministrador' => $usuario->indicadorUsuarioAdministrador)); */
+    }
+
+    public function indexAction() {
+        //$form = new FormLogin();
+        
+        $this->view->form = new FormLogin();
+    }
+}
